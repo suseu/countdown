c9fbfc9a3da55dd633ffb740115b6b9c1118f035
Michal Suski <michal.suski@ge.com>
2013-06-14 15:39:33 +0200

    -added onBeforeExpiry callback

--- jquery.countdown.js
+++ jquery.countdown.js
@@ -1,7 +1,7 @@
 /* http://keith-wood.name/countdown.html
    Countdown for jQuery v1.6.2.
    Written by Keith Wood (kbwood{at}iinet.com.au) January 2008.
-   Available under the MIT (https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt) license. 
+   Available under the MIT (https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt) license.
    Please attribute the author if you use it. */
 
 /* Display a countdown timer.
@@ -44,6 +44,9 @@ function Countdown() {
 		expiryUrl: '', // A URL to load upon expiry, replacing the current page
 		expiryText: '', // Text to display upon expiry, replacing the countdown
 		alwaysExpire: false, // True to trigger onExpiry even if never counted down
+		onBeforeExpiry: null, // Callback when the countdown almost expires -
+			// receives no parameters and 'this' is the containing division
+    beforeExpireTime: 60,
 		onExpiry: null, // Callback when the countdown expires -
 			// receives no parameters and 'this' is the containing division
 		onTick: null, // Callback when the countdown is updated -
@@ -114,7 +117,7 @@ $.extend(Countdown.prototype, {
 
 	/* List of currently active countdown targets. */
 	_timerTargets: [],
-	
+
 	/* Override the default settings for all instances of the countdown widget.
 	   @param  options  (object) the new settings to use as defaults */
 	setDefaults: function(options) {
@@ -261,6 +264,14 @@ $.extend(Countdown.prototype, {
 				inst.options.onTick.apply(target, [periods]);
 			}
 		}
+    if ($.isFunction(inst.options.onBeforeExpiry)) {
+      var almostExpired = inst._hold != 'pause' &&
+        (inst._since ? inst._now.getTime() < inst._since.getTime() - inst.options.beforeExpireTime * 1000 :
+         inst._now.getTime() >= inst._until.getTime() - inst.options.beforeExpireTime * 1000);
+      if (almostExpired) {
+        inst.options.onBeforeExpiry.apply(target, []);
+      }
+    }
 		var expired = inst._hold != 'pause' &&
 			(inst._since ? inst._now.getTime() < inst._since.getTime() :
 			inst._now.getTime() >= inst._until.getTime());
@@ -308,7 +319,7 @@ $.extend(Countdown.prototype, {
 			}
 		}
 	},
-	
+
 	/* Calculate interal settings for an instance.
 	   @param  target  (element) the containing division
 	   @param  inst    (object) the current settings for this instance
@@ -395,7 +406,7 @@ $.extend(Countdown.prototype, {
 				inst[inst._since ? '_since' : '_until'] =
 					this._determineTime(sign + inst._periods[0] + 'y' +
 						sign + inst._periods[1] + 'o' + sign + inst._periods[2] + 'w' +
-						sign + inst._periods[3] + 'd' + sign + inst._periods[4] + 'h' + 
+						sign + inst._periods[3] + 'd' + sign + inst._periods[4] + 'h' +
 						sign + inst._periods[5] + 'm' + sign + inst._periods[6] + 's');
 				this._addTarget(target);
 			}
@@ -445,7 +456,7 @@ $.extend(Countdown.prototype, {
 					case 'd': day += parseInt(matches[1], 10); break;
 					case 'w': day += parseInt(matches[1], 10) * 7; break;
 					case 'o':
-						month += parseInt(matches[1], 10); 
+						month += parseInt(matches[1], 10);
 						day = Math.min(day, plugin._getDaysInMonth(year, month));
 						break;
 					case 'y':
@@ -530,8 +541,8 @@ $.extend(Countdown.prototype, {
 			inst.options.compact, inst.options.significant, showSignificant) :
 			((inst.options.compact ? // Compact version
 			'<span class="' + this._rowClass + ' ' + this._amountClass +
-			(inst._hold ? ' ' + this._holdingClass : '') + '">' + 
-			showCompact(Y) + showCompact(O) + showCompact(W) + showCompact(D) + 
+			(inst._hold ? ' ' + this._holdingClass : '') + '">' +
+			showCompact(Y) + showCompact(O) + showCompact(W) + showCompact(D) +
 			(show[H] ? this._minDigits(inst, inst._periods[H], 2) : '') +
 			(show[M] ? (show[H] ? inst.options.timeSeparator : '') +
 			this._minDigits(inst, inst._periods[M], 2) : '') +
@@ -656,7 +667,7 @@ $.extend(Countdown.prototype, {
 		show[S] = (format.match('s') ? '?' : (format.match('S') ? '!' : null));
 		return show;
 	},
-	
+
 	/* Calculate the requested periods between now and the target time.
 	   @param  inst         (object) the current settings for this instance
 	   @param  show         (string[7]) flags indicating which periods are requested/required
